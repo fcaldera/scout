7cb2f891c1b62dec550e1bc9cbf66c1df9259f1f
Fernando Caldera <fernando@jobbatical.com>
2018-12-19 00:43:13 +0200

    Add modal component

---
+++ src/Modal.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root'); //Avoid accesibility warning
+ReactModal.defaultStyles.overlay.backgroundColor = '#282c34';
+
+const Modal = styled(ReactModal)`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: var(--primary);
+  font-size: 1.5rem;
+  padding: 2rem;
+  background: '#282c34';
+  border: none;
+`;
+
+export default Modal;
